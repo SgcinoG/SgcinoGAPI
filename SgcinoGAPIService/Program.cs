aec209e4ce533b2dcd7b80f7edaaf29d6d62fd6a
Sgcino Gwacela <Sgcinog@contour.co.za>
2022-01-17 13:37:51 +0200

    Services using Hangfire and TopShelf

---
+++ SgcinoGAPIService/Program.cs
@@ -0,0 +1,66 @@
+﻿using Hangfire;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Configuration;
+using Topshelf;
+using SgcinoGAPIBusinessLayer;
+using Microsoft.Owin.Host.HttpListener;
+
+namespace SgcinoGAPIService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<HangfireService>(servconfig =>
+                {
+                    servconfig.ConstructUsing(() => new HangfireService());
+                    servconfig.WhenStarted(s => s.Start());
+                    servconfig.WhenStopped(s => s.Stop());
+                });
+            });
+        }
+
+        public class HangfireService
+        {
+            BackgroundJobServer server;
+            private IDisposable _host;
+
+            public void Start()
+            {
+                var options = new StartOptions
+                {
+                    Port = int.Parse(ConfigurationManager.AppSettings["HangfireDashboardPort"])
+                };
+
+                _host = WebApp.Start<Startup>(options);
+
+                //Need to provide the timezone to Hangfire - If we don't, it will take a default timezone which is not SA time and will not execute jobs when we expect it to
+                var timeZone = TimeZoneInfo.FindSystemTimeZoneById("South Africa Standard Time");
+
+                //start the BackgroundJob Server
+                server = new BackgroundJobServer();
+
+                //schedule job
+                RecurringJob.AddOrUpdate("Get Daily Orders", () => new OrdersBl("").AddOrder(new SgcinoGAPIDataModels.Models.OrdersCtrl { Created = DateTime.Now, Products = new System.Collections.Generic.List<int> { 1, 2 } }), "0 12 * * *", timeZone);
+                //# ┌───────────── minute (0 - 59)
+                //# │ ┌───────────── hour (0 - 23)
+                //# │ │ ┌───────────── day of the month (1 - 31)
+                //# │ │ │ ┌───────────── month (1 - 12)
+                //# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
+                //# │ │ │ │ │                                   7 is also Sunday on some systems)
+                //# │ │ │ │ │
+                //# │ │ │ │ │
+                //# * * * * *
+            }
+
+            public void Stop()
+            {
+                server?.Dispose();
+                _host?.Dispose();
+            }
+        }
+    }
+}
---
+++ SgcinoGAPIService/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Host.HttpListener;
+
+
+namespace SgcinoGAPIService
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["SgcinoGAPIDB"].ConnectionString;
+
+            GlobalConfiguration.Configuration
+                .UseColouredConsoleLogProvider()
+                .UseSqlServerStorage(connString);
+            app.UseHangfireDashboard();
+        }
+
+    }
+}
